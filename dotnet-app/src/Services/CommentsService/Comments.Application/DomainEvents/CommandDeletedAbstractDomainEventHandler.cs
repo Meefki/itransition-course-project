8b564f5537d619098b674670c0effa9dd1c3a9ac
Vitaly Akindinov <vitaly@mail.maxxle.com>
2023-08-22 22:35:07 +0400

    add event placeholder

---
+++ dotnet-app/src/Services/CommentsService/Comments.Application/DomainEvents/CommandDeletedAbstractDomainEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Comments.Application.SeedWork;
+using Comments.Domain.DomainEvents;
+using Microsoft.Extensions.Logging;
+
+namespace Comments.Application.DomainEvents;
+
+public class CommandDeletedAbstractDomainEventHandler<TRequest>
+    : NotificationHandler<TRequest>
+    where TRequest : CommentDeletedDomainEvent
+{
+    public CommandDeletedAbstractDomainEventHandler(ILogger logger) 
+        : base(logger)
+    {
+    }
+
+    protected override Task Action(TRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
