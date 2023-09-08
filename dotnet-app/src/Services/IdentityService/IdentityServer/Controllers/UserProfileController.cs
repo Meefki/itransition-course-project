1fddb469e700fc90cc529985bc869ed4522dc968
Vitaly Akindinov <vitaly@mail.maxxle.com>
2023-09-08 18:42:11 +0400

    add user profile controller

---
+++ dotnet-app/src/Services/IdentityService/IdentityServer/Controllers/UserProfileController.cs
@@ -0,0 +1,64 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly string connectionString;
+
+        public UserProfileController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("Authorization")!;
+        }
+
+        [HttpGet]
+        [Route("names")]
+        public async Task<dynamic> GetUserNames([FromQuery] List<string> ids)
+        {
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            var timeout = TimeSpan.FromSeconds(10);
+
+            if (ids.Any())
+            {
+                object param = new { };
+                string sql =
+    @$"select u.Id as id, u.UserName as userName
+    from [dbo].[users] as u
+    where u.id in ({string.Join(',', ids.Select(x => $"'{x}'"))})";
+                IEnumerable<dynamic> result = await connection.QueryAsync<dynamic>(sql, param, commandTimeout: timeout.Seconds);
+                return result;
+            }
+
+            return Array.Empty<dynamic>();
+        }
+
+        [HttpGet]
+        public async Task<dynamic> GetUserProfile(string id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            var timeout = TimeSpan.FromSeconds(10);
+
+            if (!string.IsNullOrEmpty(id))
+            {
+                object param = new { };
+                string sql =
+    @$"select top 1
+         u.Id       as id
+        ,u.UserName as userName
+        ,u.Email    as email
+    from [dbo].[users] as u
+    where u.id in ('{id}')";
+                dynamic result = await connection.QueryAsync<dynamic>(sql, param, commandTimeout: timeout.Seconds);
+                return result is null ? new { } : (result[0] ?? new { });
+            }
+
+            return new { };
+        }
+    }
+}
