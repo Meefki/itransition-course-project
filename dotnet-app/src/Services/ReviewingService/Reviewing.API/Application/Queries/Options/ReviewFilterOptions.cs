e32e2dee4ad98f37c45d7c6ad5b1273916be541f
Vitaly Akindinov <vitaly@mail.maxxle.com>
2023-09-08 18:34:26 +0400

    add filtering by content

--- dotnet-app/src/Services/ReviewingService/Reviewing.API/Application/Queries/Options/ReviewFilterOptions.cs
+++ dotnet-app/src/Services/ReviewingService/Reviewing.API/Application/Queries/Options/ReviewFilterOptions.cs
@@ -3,6 +3,7 @@
 public class ReviewFilterOptions
 {
     public string? Name { get; set; } = null;
+    public string? Content { get; set; } = null;
     public string? SubjectName { get; set; } = null;
     public string? Status { get; set; } = null;
     public List<string>? Tags { get; set; } = null;
