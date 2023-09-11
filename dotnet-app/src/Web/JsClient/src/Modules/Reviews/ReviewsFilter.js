1cb52626f98e17263f542e860dd309c4132a4397
Vitaly Akindinov <vitaly@mail.maxxle.com>
2023-09-11 19:09:29 +0400

    add translation to filters of review

--- dotnet-app/src/Web/JsClient/src/Modules/Reviews/ReviewsFilter.js
+++ dotnet-app/src/Web/JsClient/src/Modules/Reviews/ReviewsFilter.js
@@ -1,5 +1,37 @@
-function ReviewsFilter() {
-    return(
+import { useContext, useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { FilterOptionsContext } from "../../Contexts/FilterOptionsContext";
+// import { useNavigate } from "react-router-dom";
+
+function ReviewsFilter({ immutableFilters = [] }) {
+    const { filterOptions, setFilterOptions } = useContext(FilterOptionsContext);
+    const immutableFilterOptions = immutableFilters?.map((option) => { 
+        return { 
+            name: option, 
+            value: filterOptions[option]
+        }
+    });
+
+    const ns = "user-profile";
+    const { t, i18n } = useTranslation(ns);
+    const [pageLoadingStage, setPageLoadingStage] = useState(true);
+    //const navigate = useNavigate();
+
+    /* eslint-disable */
+    useMemo(() => {
+        i18n.isInitialized &&
+        !i18n.hasLoadedNamespace(ns) && 
+            i18n.loadNamespaces(ns)
+            .then(() => {
+                setPageLoadingStage(false);
+            });
+        i18n.isInitialized &&
+        i18n.hasLoadedNamespace(ns) &&
+            setPageLoadingStage(false);
+    }, [i18n.isInitialized]);
+    /* eslint-enable */
+
+    return pageLoadingStage ? '' :
         <div>
             Filter
             {/* dropdown with tags */}
@@ -7,7 +39,6 @@ function ReviewsFilter() {
             {/* publish date */}
             {/*  */}
         </div>
-    );
 }
 
 export default ReviewsFilter;
