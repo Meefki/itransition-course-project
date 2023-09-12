6976c261c3ea71494d9b5a7ae4b13345c4a9fa49
Vitaly Akindinov <vitaly@mail.maxxle.com>
2023-09-12 19:46:31 +0400

    rewrote query building with URLSearchParams

--- dotnet-app/src/Web/JsClient/src/Services/QueryHelper.js
+++ dotnet-app/src/Web/JsClient/src/Services/QueryHelper.js
@@ -5,42 +5,34 @@ export const fetchGet = async (url, headers = {}) => {
 }
 
 export const getSearch = (params) => {
-    const searchParams = new URLSearchParams();
+    let searchParams = new URLSearchParams();
 
     const notNullParams = 
-    params
-    .filter(param => param?.value)
-    .filter(param => !(JSON.stringify(param?.value) === '{}'));
+        params
+        .filter(param => param?.value)
+        .filter(param => !(JSON.stringify(param?.value) === '{}'));
 
-    let searchString = '';
     if (notNullParams && notNullParams.length > 0) {
-        const searchStr = notNullParams.map(param => {
+        notNullParams.forEach(param => {
             if (Array.isArray(param?.value)) {
-                // console.log("param", param, param.value.length);
-                const params = param.value.map((val, index) => {
-                    // console.log("val:", val);
-                    let str = `${param.name}[${val.name ? val.name : index}]=`;
-                    const valueStr = Object.getOwnPropertyNames(val).includes('value') ? val.value : val;
-                    if (valueStr)
-                        str += valueStr;
-                    else
-                        str = '';
-                    // console.log(str);
-                    return str;
-                }).filter(s => s ? true : false);
-                return (params && params.length !== 0) ? params.join('&') : '';
+                param.value.forEach((val, index) => {
+                    const name = `${param.name}[${val.name ? val.name : index}]`;
+                    const value = Object.getOwnPropertyNames(val).includes('value') ? val.value : val;
+                    searchParams.append(name, value);
+                });
             }
-            else
-                return param.name + '=' + param.value
+            else {
+                searchParams.append(param.name, param.value);
+            }
         });
-        searchString = searchStr.filter(s => s ? true : false) ? searchStr.filter(s => s ? true : false).join('&') : '';
     }
 
-    if (searchString)
-        searchString = '?' + searchString;
+    let searchString = '';
+    console.log(searchParams.toString(), searchParams);
+    if (searchParams.toString())
+        searchString = '?' + searchParams.toString();
     else
         searchString = '';
 
-    // console.log(searchString);
     return searchString
 }
